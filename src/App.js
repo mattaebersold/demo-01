5857e4141345d3a007406753f41a0e1fd7276321
Matthew Aebersold <matt.aebersold@bkwld.com>
2024-10-07 15:36:34 -0700

    text on home page

--- src/App.js
+++ src/App.js
@@ -6,6 +6,19 @@ function App() {
   return (
     <div>
      <Header />
+
+     <div className="my-[150px] px-12 text-primary dark:text-white">
+     <p className="font-bold mt-4">On this demo:</p>
+      <ul>
+        <li>light/dark mode</li>
+        <li>Glow button</li>
+      </ul>
+
+      <p className="font-bold mt-4">Next</p>
+      <ul>
+        <li>parallax mapping</li>
+      </ul>
+     </div>
      <Nav />
     </div>
   );
